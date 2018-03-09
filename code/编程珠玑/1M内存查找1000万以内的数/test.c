5c5b23f9aa53c0f4ac402d374a734001bb0e2c51
duanyong <coderduan@gmail.com>
2018-03-09 22:30:47 +0800

    not done c 1

--- code/编程珠玑/1M内存查找1000万以内的数/test.c
+++ code/编程珠玑/1M内存查找1000万以内的数/test.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+#define BITSPERWORD 32      //由于一个int采用4字节32位bit，
+#define SHIFT       5       //由于2^5为32，所以设置数字的位移量
+#define MASK        0x1F    //用于取余操作，如  & MASK = 
+#define N           1000000
+
+
 void main(int argc, char * argv[]) {
+    int bitmap[(N -1) / BITSPEWORD];
+
+    void SET(int i) {        bitmap[i >> SHIFT] |=  ( 1 << ( i & MASK) ); }
+    void CLR(int i) {        bitmap[i >> SHIFT] &= ~( 1 << ( i & MASK) ); }
+    void test(int i){ return bitmap[i >> SHIFT] &   ( 1 << ( i & MASK )); }
 
     FILE *fp = fopen("./test", "w");
 
